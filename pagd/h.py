7c45483a8ef039da3fe9a9b0f213e9b9219cba7a
Pratap Chakravarthy <prataprc@gmail.com>
2013-06-17 11:17:40 +0530

    Added a helper module ``pagd.h`` to contain helper functions for tayra templates. All functions inside this module will automatically be made available under ``h`` namespace inside ttl-script.

---
+++ pagd/h.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE', which is part of this source code package.
+#       Copyright (c) 2013 R Pratap Chakravarthy
+
+"""Helper functions for pagd's layout templates."""
+
+import sys
+from   os.path      import dirname, join
+
+__all__ = [ 'readfile' ]
+
+#---- Generic helper functions.
+
+def readfile( relpath ):
+    """Read file from path ``relpath`` relative to calling template."""
+    frame = sys._getframe(1)
+    if frame.f_code.co_name in core_classes : return # Skip
+    filen = frame.f_globals.get('_ttlfile', None)
+    if isfile( filen ) :
+        f = join( dirname(filen), relpath )
+        if isfile( f ) :
+            return open(f).read()
+    return ''
